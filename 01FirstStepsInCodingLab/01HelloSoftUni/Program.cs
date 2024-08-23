5c605d0c6c4a1cd46bd2717ba3c693bcc72b5ca5
aleksandarMilev <148962849+aleksandarMilev@users.noreply.github.com>
2024-08-23 23:48:37 +0300

    Add files via upload

---
+++ 01FirstStepsInCodingLab/01HelloSoftUni/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _01HelloSoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello SoftUni");
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            char temp = firstChar;
+            firstChar = secondChar;
+            secondChar = temp;
+
+            Console.WriteLine(firstChar);
+            Console.WriteLine(secondChar);
+        }
+    }
+}
